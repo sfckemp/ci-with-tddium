dec9d2609ac8368648b7b6718755cd5ee9159717
Chris Kemp <ckemp@salesforce.com>
2012-06-30 17:50:54 -0400

    in right dir

---
+++ test/web_test.rb
@@ -0,0 +1,14 @@
+require 'web'
+require 'test/unit'
+require 'rack/test'
+
+set :environment, :test
+
+class MySimpleTest < Test::Unit::TestCase
+
+    def test_basic
+        browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
+        browser.get '/', :name => 'Brad'
+        assert_equal 'Hello, Brad', browser.last_response.body
+    end
+end
