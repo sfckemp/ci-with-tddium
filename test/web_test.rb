4044864f5c92477c871ef0e68fe6bc5f422358a7
Chris Kemp <ckemp@salesforce.com>
2012-06-30 20:12:38 -0400

    test change

--- test/web_test.rb
+++ test/web_test.rb
@@ -9,6 +9,6 @@ class MySimpleTest < Test::Unit::TestCase
     def test_basic
         browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
         browser.get '/', :name => 'Brad'
-        assert_equal 'Hello, Brad', browser.last_response.body
+        assert_equal 'Hello, Brad!', browser.last_response.body
     end
 end
